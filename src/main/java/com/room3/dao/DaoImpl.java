41133565037835b7f8dc27bb7a852445f3a3d870
critta-montes <christopher@revature.net>
2022-03-11 17:04:48 -0500

    1st function

--- src/main/java/com/room3/dao/DaoImpl.java
+++ src/main/java/com/room3/dao/DaoImpl.java
@@ -18,12 +18,16 @@ public class DaoImpl {
 	private Configuration conn;
 	Configuration cfg = new Configuration();
 
-	List<MetaModel<Class<?>>> meta = cfg.getMetaModels();
-	List<PrimaryKeyField> pkFields = (List<PrimaryKeyField>) ((MetaModel<Class<?>>) meta).getPrimaryKey();
-	List<ColumnField> columns = ((MetaModel<Class<?>>) meta).getColumns();
-	List<ForeignKey> foreignKeyFields = ((MetaModel<Class<?>>) meta).getForeignKeys();
 
-	public <T> int insert(Class<?> clazz, T t) {
+	public <T> int insert(Object o) {
+		
+		Class<?> clazz = o.getClass();
+		MetaModel<T>  mta = new MetaModel(clazz);
+		//List<MetaModel<Class<?>>> meta = cfg.getMetaModels();
+		PrimaryKeyField pkFields = mta.getPrimaryKey();
+		List<ColumnField> columns = mta.getColumns();
+		//List<ForeignKey> foreignKeyFields = mta.getForeignKeys();
+
 		try {
 			Connection con = Configuration.getConnection();
 
@@ -47,47 +51,57 @@ public class DaoImpl {
 			}
 			insertCommand.append(");");
 			String sql = insertCommand.toString();
+			System.out.println(sql);
 			PreparedStatement stmt = con.prepareStatement(sql);
 			int index = 1;
 
 			for (ColumnField f : columns) {
-
+				String name =f.getName();
+				System.out.println(name);
 				Field field = null;
+		        try {
+		            field = o.getClass().getDeclaredField(name);
+		            System.out.println(field);
+		            field.setAccessible(true);
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		        assert field != null;
 				String fieldType = f.getType().getSimpleName();
 
-				field.setAccessible(true);
+				
 				try {
 					switch (fieldType) {
 					case "int":
-						stmt.setInt(index, (int) field.get(clazz));
+						stmt.setInt(index, (int) field.get(o));
 						index++;
 						break;
 					case "String":
-						stmt.setString(index, (String) field.get(clazz));
+						stmt.setString(index, (String) field.get(o));
 						index++;
 						break;
 					case "boolean":
-						stmt.setBoolean(index, (boolean) field.get(clazz));
+						stmt.setBoolean(index, (boolean) field.get(o));
 						index++;
 						break;
 					case "double":
-						stmt.setDouble(index, (double) field.get(clazz));
+						stmt.setDouble(index, (double) field.get(o));
 						index++;
 						break;
 					case "byte":
-						stmt.setByte(index, (byte) field.get(clazz));
+						stmt.setByte(index, (byte) field.get(o));
 						index++;
 						break;
 					case "float":
-						stmt.setFloat(index, (float) field.get(clazz));
+						stmt.setFloat(index, (float) field.get(o));
 						index++;
 						break;
 					case "long":
-						stmt.setLong(index, (long) field.get(clazz));
+						stmt.setLong(index, (long) field.get(o));
 						index++;
 						break;
 					case "short":
-						stmt.setShort(index, (short) field.get(clazz));
+						stmt.setShort(index, (short) field.get(o));
 						index++;
 						break;
 
---
+++ src/main/java/com/room3/demos/DummyUser.java
@@ -0,0 +1,71 @@
+package com.room3.demos;
+
+import java.util.Objects;
+
+import com.room3.annotations.Column;
+import com.room3.annotations.Entity;
+import com.room3.annotations.Id;
+
+@Entity(tableName = "DummyUser")
+public class DummyUser {
+	@Column(columnName = "")
+	@Id(columnName = "user_id")
+	private int user_id;
+	@Column(columnName = "username")
+	private String username;
+	@Column(columnName = "passd")
+	private String passd;
+	public DummyUser(int user_id, String username, String passd) {
+		super();
+		this.user_id = user_id;
+		this.username = username;
+		this.passd = passd;
+	}
+	public DummyUser(String username, String passd) {
+		super();
+		this.username = username;
+		this.passd = passd;
+	}
+	
+	public DummyUser() {
+		super();
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return passd;
+	}
+	public void setPass(String pass) {
+		this.passd = passd;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(passd, user_id, username);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DummyUser other = (DummyUser) obj;
+		return Objects.equals(passd, other.passd) && user_id == other.user_id && Objects.equals(username, other.username);
+	}
+	
+	
+	
+	
+}
+

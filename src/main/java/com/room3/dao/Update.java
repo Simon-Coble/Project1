28b445c5113e4593fdcf90f79a40d21ccd477dc6
Simon-Coble <simon291@revature.net>
2022-03-12 14:46:00 -0500

    bugtastic

--- src/main/java/com/room3/dao/Update.java
+++ src/main/java/com/room3/dao/Update.java
@@ -1,7 +1,13 @@
 package com.room3.dao;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.room3.annotations.Column;
@@ -11,27 +17,75 @@ import com.room3.util.Configuration;
 
 public class Update {
 	private Class<?> clazz;
-    private boolean isTransaction;
-    private List<Boolean> completes;
-    Field[] fields = clazz.getDeclaredFields();
-    Connection con = Configuration.getConnection();
-    Entity table = clazz.getDeclaredAnnotation(Entity.class);
-    
-    public boolean update(Object o) {
-    	StringBuilder sql = new StringBuilder();
-    	sql.append("UPDATE " + table.tableName() + " SET ");
-    	for (Field field :fields)
-    	{
-    		sql.append(clazz.getDeclaredAnnotation(Column.class) + " = " + field.getName() + " ");
-    	}
-    	sql.append( "WHERE id = " + clazz.getDeclaredAnnotation(Id.class));
-    	
-    	return true;
-    }
-    
-    public boolean delete(Object o) {
-    	StringBuilder sql = new StringBuilder();
-    	sql.append("DELETE FROM " + table.tableName() + " WHERE id = " + clazz.getDeclaredAnnotation(Id.class));
-    	return true;
-    }
+	private boolean isTransaction;
+	private List<Boolean> completes;
+
+	Connection con = Configuration.getConnection();
+	public List<Object> flubber = new ArrayList<Object>();
+
+
+
+	public boolean update(Object o) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("UPDATE " + table.tableName() + " SET ");
+		for (Field field : fields) {
+			sql.append(clazz.getDeclaredAnnotation(Column.class) + " = " + field.getName() + " ");
+		}
+		sql.append("WHERE id = " + clazz.getDeclaredAnnotation(Id.class));
+
+		return true;
+	}
+
+	public boolean delete(Object o) {
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM " + table.tableName() + " WHERE id = " + clazz.getDeclaredAnnotation(Id.class));
+		return true;
+	}
+
+	public List<Object> findAll(Object o) throws SQLException, NoSuchMethodException, SecurityException {
+		clazz = o.getClass();
+		Entity table = clazz.getDeclaredAnnotation(Entity.class);
+		StringBuilder far = new StringBuilder();
+		far.append("SELECT * FROM " + table.tableName().toLowerCase());
+		String sql = far.toString();
+		System.out.println(sql);
+		PreparedStatement stmt = con.prepareStatement(sql);
+		ResultSet rs;
+		if ((rs = stmt.executeQuery()) != null) {
+			while (rs.next()) {
+				
+				flubber.add(getGetter(rs));
+			}
+		}
+		return flubber;
+	}
+	public Object getGetter(Object o) {
+		Method[] methods = o.getClass().getDeclaredMethods();
+	for(Method method : methods){
+	      if(isGetter(method)){
+	        try {
+	          return method.invoke(o);
+	        } catch (IllegalAccessException e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+	        } catch (IllegalArgumentException e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+	        } catch (InvocationTargetException e) {
+	          // TODO Auto-generated catch block
+	          e.printStackTrace();
+	        }
+	      }
+	}
+	return methods;
+	}
+	
+	  private static boolean isGetter(Method method){
+		    // identify get methods
+		    if((method.getName().startsWith("get") || method.getName().startsWith("is")) 
+		        && method.getParameterCount() == 0 && !method.getReturnType().equals(void.class)){
+		      return true;
+		    }
+		    return false; 
+	  }
 }

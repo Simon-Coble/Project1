760132cb8825332403bf8d674d5b9ff91dbb9142
critta-montes <christopher@revature.net>
2022-03-12 16:28:41 -0500

    constructor function

--- src/main/java/com/room3/dao/SelectById.java
+++ src/main/java/com/room3/dao/SelectById.java
@@ -2,6 +2,7 @@ package com.room3.dao;
 
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,10 +14,12 @@ import com.room3.util.Configuration;
 import com.room3.util.ForeignKey;
 import com.room3.util.MetaModel;
 import com.room3.util.PrimaryKeyField;
+import org.apache.commons.beanutils.ConstructorUtils;
+
 public class SelectById {
 
 	
-	public <T> void selectById(Object o, int id) {
+	public <T> void selectById(Object o, int id) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 		
 	
 	Class<?> clazz = o.getClass();
@@ -28,18 +31,101 @@ public class SelectById {
 	try {
 		Connection con = Configuration.getConnection();
 		
+		StringBuilder sqlCommand = new StringBuilder();
+		sqlCommand.append("select * from ");
+		String tableName = clazz.getSimpleName().toLowerCase();
+		sqlCommand.append(tableName);
+		sqlCommand.append(" where ");
+		String idName = pkFields.getColumnName();
+		sqlCommand.append(idName);
+		sqlCommand.append(" = ");
+		sqlCommand.append(id);
+		String sql = sqlCommand.toString();
+		PreparedStatement stmt = con.prepareStatement(sql);
+		
+		ResultSet rs;
+		if((rs = stmt.executeQuery()) != null) {
+			while (rs.next()) {
+				Object b =createNewInstance(clazz.getName());
+				System.out.println(b);
+				for (ColumnField f : columns) {
+					String name =f.getName();
+					System.out.println(name);
+					Field field = null;
+					 String fieldType = f.getType().getSimpleName();
+					try {
+			            field = b.getClass().getDeclaredField(name);
+			            System.out.println(field);
+			            field.setAccessible(true);
+			            
+			           
+							switch (fieldType) {
+							case "int":
+								field.setInt(b, id);
+								break;
+							case "String":
+								field.set(b, name);
+								break;
+							}
+							System.out.println(b.toString());
+//							case "oolean":
+//								
+//								break;
+//							case "double":
+//								
+//								break;
+//							case "byte":
+//								
+//								break;
+//							case "float":
+//								
+//								break;
+//							case "long":
+//								
+//								break;
+//							case "short":
+//								
+//								break;
+//
+//							}
+			            
+			            
+							    
+			        } catch (Exception e) {
+			            e.printStackTrace();
+			        }
+			}
+		}
+		
+		}
+		
 		
 	
 	
 	} catch (IllegalArgumentException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
-	} catch (IllegalAccessException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+	
+	
+	
+	
 }
+	private Object createNewInstance(String clazzName) {
+		  Class<?> beanClass = null;
+		  Object beanInstance = null;
+		  try {
+		    beanClass = getClass().getClassLoader().loadClass(clazzName);
+		    beanInstance = ConstructorUtils.invokeConstructor(beanClass, null);
+		  } catch (Exception e) {
+		    System.out.println("Error during creating class" + clazzName);
+		  }
+		  return beanInstance;
+		}
+	
+	
 }
